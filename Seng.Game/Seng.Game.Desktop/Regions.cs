76ba6e5a753f57cd007e928b0cf1b40993056128
Martin Hořelka <xhorelka@fi.muni.cz>
2020-03-28 19:35:23 +0100

    BrowserModule initial

--- Seng.Game/Seng.Game.Desktop/Regions.cs
+++ Seng.Game/Seng.Game.Desktop/Regions.cs
@@ -13,6 +13,8 @@ namespace Seng.Game.Desktop
 
 		public static readonly string EmailRegion = "EmailRegion";
 
+		public static readonly string BrowserRegion = "BrowserRegion";
+
 		#endregion
 
 		#region Region Views
@@ -20,19 +22,25 @@ namespace Seng.Game.Desktop
 		public static readonly string GameView = nameof(Views.GameView);
 		public static readonly Type GameViewType = typeof(GameView);
 
+
 		public static readonly string PauseView = nameof(Views.PauseView);
 		public static readonly Type PauseViewType = typeof(PauseView);
+
 
 		public static readonly string IntermissionModuleView = nameof(Views.IntermissionModuleView);
 		public static readonly Type IntermissionModuleViewType = typeof(IntermissionModuleView);
 
+
 		public static readonly string DesktopModuleView = nameof(Views.DesktopModuleView);
 		public static readonly Type DesktopModuleViewType = typeof(DesktopModuleView);
 
+
 		public static readonly string BrowserModuleView = nameof(Views.BrowserModuleView);
+		public static readonly string MinigameSelectionView = nameof(Views.MinigameSelectionView);
+		public static readonly Type MinigameSelectionViewType = typeof(MinigameSelectionView);
+
 
 		public static readonly string EmailModuleView = nameof(Views.EmailModuleView);
-
 		public static readonly string NewEmailView = nameof(Views.NewEmailView);
 		public static readonly string DisplayEmailView = nameof(Views.DisplayEmailView);
 		public static readonly string EmptyEmailView = nameof(Views.EmptyEmailView);
---
+++ Seng.Game/Seng.Game.Desktop/ViewModels/BrowserModule/MinigameSelectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Events;
+using Prism.Regions;
+using Seng.Game.Desktop.ViewModels.Base;
+
+namespace Seng.Game.Desktop.ViewModels
+{
+	public class MinigameSelectionViewModel : BaseViewModel
+	{
+		public MinigameSelectionViewModel(IRegionManager regionManager, IEventAggregator eventAggregator, GameState gameState)
+			: base(regionManager, eventAggregator, gameState)
+		{
+		}
+	}
+}
