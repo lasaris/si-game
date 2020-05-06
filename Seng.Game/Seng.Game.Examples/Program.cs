f6bfcec2c9b03e61b106b6ad2c33cbbcbf25c1b9
stefan.bojnak <stefan.bojnak@notino.com>
2020-05-06 21:06:49 +0200

    Mail test data.

--- Seng.Game/Seng.Game.Examples/Program.cs
+++ Seng.Game/Seng.Game.Examples/Program.cs
@@ -69,6 +69,8 @@ namespace Seng.Game.Examples
             intermissionModule = await mediator.Send(getModuleRequest);
 
             Console.WriteLine(intermissionModule.GetType());
+
+
         }
     }
 }
